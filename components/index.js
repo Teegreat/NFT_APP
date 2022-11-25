3faf1ddd605cc2d0c1b3847a66c31c3b45a92d5a
TeeGreat <toyinatofarati64@gmail.com>
2022-11-25 21:55:21 +0100

    initial commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RecButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RecButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
